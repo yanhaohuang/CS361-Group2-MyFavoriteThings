280bb7d427b8d0fcb9c3c0b0ee13f4130785834d
Jared Sulzdorf <j.sulzdorf@gmail.com>
2019-03-08 15:41:47 -0800

    got filenames showing in gridview

--- app/src/main/java/com/group2/myfavoritethings/FavoriteThings.java
+++ app/src/main/java/com/group2/myfavoritethings/FavoriteThings.java
@@ -3,12 +3,14 @@ package com.group2.myfavoritethings;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -35,28 +37,53 @@ public class FavoriteThings extends BaseAdapter {
         return position;
     }
 
+    public String getItemName( int position ){ return imagenames.get(position); }
+
 
     @Override
-    public ImageView getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        imageView = new ImageView(mContext);
-        if (convertView == null) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // https://stackoverflow.com/questions/35158652/how-to-add-textview-inside-a-gridview-filled-with-imageview
+        LinearLayout linearlayout;
+        if( convertView == null ){
 
-            imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            // Set up the linear layout view
+            linearlayout = new LinearLayout(mContext);
+            linearlayout.setOrientation(LinearLayout.VERTICAL);
 
-            imageView.setPadding(0, 0, 0, 0);
+            // Set up the image view
+            ImageView imageView;
+            imageView = new ImageView(mContext);
+            if (convertView == null) {
+
+                imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                imageView.setPadding(0, 0, 0, 0);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Bitmap b= BitmapFactory.decodeFile(imagenames.get(position));
+            imageView.setImageBitmap(b);
+
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
+
+            linearlayout.addView(imageView);
+
+            // set up the text view
+            TextView imageViewName = new TextView(mContext);
+            imageViewName.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            imageViewName.setText(getItemName( position ) );
+            imageViewName.setGravity(Gravity.CENTER);
+
+            linearlayout.addView(imageViewName);
+
         } else {
-            imageView = (ImageView) convertView;
+            linearlayout = (LinearLayout) convertView;
         }
 
-        Bitmap b= BitmapFactory.decodeFile(imagenames.get(position));
-        imageView.setImageBitmap(b);
-
-        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
-        Log.i( "Image Name", imagenames.get(position) );
-        return imageView;
+        return linearlayout;
     }
 
 }
